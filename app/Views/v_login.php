01c59661a9137023230292ea3e05b54a3b0e4b69
ansyahdotcom <55609786+ansyahdotcom@users.noreply.github.com>
2021-12-02 11:23:38 +0700

    1

---
+++ app/Views/v_login.php
@@ -0,0 +1,35 @@
+<?= $this->extend('layout/template_login') ?>
+<?= $this->section('content'); ?>
+
+<div class="login-box">
+    <!-- /.login-logo -->
+    <div class="card card-outline card-primary">
+        <div class="card-header text-center">
+            <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
+        </div>
+        <div class="card-body">
+
+            <form action="login/login" method="post">
+                <?= csrf_field() ?>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Username" name="username" required autocomplete="off">
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" placeholder="Password" name="password" required>
+                </div>
+                <div class="row">
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+    <a href="dashboard" type="button" class="btn btn-primary">ke dashboard</a>
+</div>
+<!-- /.login-box -->
+
+<?= $this->endSection(); ?>
